2609a60cc72d2652d80db57d59d53fc2fa1daf30
kukoza <gglike21@gmail.com>
2025-05-07 08:44:53 +0700

    sw

--- server.js
+++ server.js
@@ -1,13 +1,19 @@
 const { createServer } = require('http');
 const next = require('next');
 
-const app = next({ dev: false });
+const dev = process.env.NODE_ENV !== 'production';
+const app = next({ dev });
 const handle = app.getRequestHandler();
+
+// ใช้พอร์ตจาก environment variable หรือใช้ค่าเริ่มต้น 3000
+const port = process.env.PORT || 3000;
+const hostname = process.env.HOSTNAME || '0.0.0.0';
 
 app.prepare().then(() => {
   createServer((req, res) => {
     handle(req, res);
-  }).listen(3000, () => {
-    console.log('Server is running on http://localhost:3000');
+  }).listen(port, hostname, (err) => {
+    if (err) throw err;
+    console.log(`> Server running on http://${hostname}:${port}`);
   });
 });
